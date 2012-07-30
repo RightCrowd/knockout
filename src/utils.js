2e3decb1c2a51b7cabcdec57b788515c251f0aa5
NikGovorov <nikita.govorov@gmail.com>
2012-07-30 13:20:25 +1000

    Fixed memory leaks in IE 6/7/8: jquery supports 'propertychange' event for all versions of IE < 9, so it is preferable to use jquery event model by default(if jquery is defined) to avoid memory leaks.

--- src/utils.js
+++ src/utils.js
@@ -13,7 +13,6 @@ ko.utils = new (function () {
                 knownEventTypesByEventName[knownEventsForType[i]] = eventType;
         }
     }
-    var eventsThatMustBeRegisteredUsingAttachEvent = { 'propertychange': true }; // Workaround for an IE9 issue - https://github.com/SteveSanderson/knockout/issues/406
 
     // Detect IE versions for bug workarounds (uses IE conditionals, not UA string, for robustness)
     var ieVersion = (function() {
@@ -28,6 +27,12 @@ ko.utils = new (function () {
     }());
     var isIe6 = ieVersion === 6,
         isIe7 = ieVersion === 7;
+
+	var eventsThatMustBeRegisteredUsingAttachEvent = {};
+    
+    if (ieVersion == 9) {
+        eventsThatMustBeRegisteredUsingAttachEvent = { 'propertychange': true }; // Workaround for an IE9 issue - https://github.com/SteveSanderson/knockout/issues/406
+    }
 
     function isClickOnCheckableElement(element, eventType) {
         if ((ko.utils.tagNameLower(element) !== "input") || !element.type) return false;
